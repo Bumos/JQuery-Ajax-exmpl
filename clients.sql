694a2bf17fe45174241a22a870581666484c6105
Bumos <Bumos@yandex.ru>
2017-12-19 22:41:03 +0300

    Add files via upload

---
+++ clients.sql
@@ -0,0 +1,49 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 3.5.0
+-- http://www.phpmyadmin.net
+--
+-- Хост: localhost
+-- Время создания: Дек 19 2017 г., 20:38
+-- Версия сервера: 5.1.62-community
+-- Версия PHP: 5.3.27
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `phpmyadmin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `clients`
+--
+
+CREATE TABLE IF NOT EXISTS `clients` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
+
+--
+-- Дамп данных таблицы `clients`
+--
+
+INSERT INTO `clients` (`id`, `firstname`, `lastname`, `phone`) VALUES
+(8, 'Иван', 'Иванович', ''),
+(9, 'Петр', 'Петрович', ''),
+(10, 'Александр', 'Николаевич', ''),
+(11, 'Михаил', 'Витальевич', '');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
